0c77e501c30b90e7278568d6759248c9db4b2a02
Ibrahima Diallo <111662876+IbrahimaDiallo32@users.noreply.github.com>
2023-10-11 14:49:51 -0400

    Add files via upload

---
+++ AlienLanguage/main (20).cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+
+class Program {
+  public static void Main (string[] args) {
+    for(int i=0;i<5;i++) {
+  FakeCharacter myChar=new FakeCharacter(5,10);
+  myChar.printCharacter();
+  Console.WriteLine("~~~~~~~~~~");
+}
+}
+  }
+  
