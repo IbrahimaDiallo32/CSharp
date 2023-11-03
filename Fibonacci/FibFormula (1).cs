4736845cd2b1175b15f9118fb2ef1cda676cfb35
Ibrahima Diallo <111662876+IbrahimaDiallo32@users.noreply.github.com>
2023-11-03 12:35:16 -0400

    Update FibFormula (1).cs

--- Fibonacci/FibFormula (1).cs
+++ Fibonacci/FibFormula (1).cs
@@ -4,7 +4,7 @@ public class FibFormula : IFindFib{
     double y= (Math.Sqrt(5));
     double plus = (1+y)/2f;
     double minus= (1-y)/2f;
-    //split it up the parenthesis were confusing 
+    //splitting up using parenthesis is easier
     int final= Convert.ToInt32((Math.Pow(plus,z)- Math.Pow(minus,z))/ y);
     return final;
   }
